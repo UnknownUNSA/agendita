0f5b157642d9ea9ceafd480bbf7228708e96b449
notch986 <diegohh1998@gmail.com>
2021-08-10 16:59:26 -0500

    solucionado problema contacto

--- index.php
+++ index.php
@@ -1,62 +1,56 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
-	<title>Agenda de contactos web</title>
-	<link rel="icon" type="image/jpg" href="public/img/favicon.jpg">
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<?php require_once "dependencias.php"; ?>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agenda contactos</title>
+    <?php 
+    
+    require_once "dependencias.php";
+    ?>
 </head>
+
 <body>
-	<div class="container">
-		<?php require_once "menu.php"; ?>
+    <div class="container">
+        <?php require_once "menu.php";?>
+        <div class="jumbotron">
+            <h1 class="display-4">Agenda de Contactos</h1>
+            <p class="lead">Una simple agenda donde podras agregar a todos tus conocidos.</p>
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="card">
+                        <div class="card-body text-center">
+                            <img src="public/img/chupetin.jpg" class="img-fluid " alt="perfil">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <table class="table table-hover">
+                                <tr>
+                                    <th>Datos de persona, falta1</th>
+                                </tr>
+                                <tr>
+                                    <th>Datos de persona, falta2</th>
+                                </tr>
+                                <tr>
+                                    <th>Datos de persona, falta3</th>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
 
-		<div class="jumbotron">
-			<h1 class="display-4">Agenda de contactos con PHP y MySQL</h1>
+                </div>
+            </div>
+            <hr class="my-4">
+            <p>Creado con ganas por el grupo de Programación Web 1 </p>
 
-			<div class="row">
-				<div class="col-sm-6">
-					<div class="card">
-						<div class="card-body">
-							<img src="public/img/foto.png" class="img-fluid" alt="Responsive image">
-						</div>
-					</div>
-				</div>
-				<div class="col-sm-6">
-					<div class="card">
-						<div class="card-body">
-							<table class="table table-hover">
-								<tr>
-									<th>
-										Agenda desarrollada en el año 2021
-									</th>
-								</tr>
-								<tr>
-									<th>
-										Hecha por Facultad autodidacta
-									</th>
-								</tr>
-								<tr>
-									<th>
-										Hecho con php y mysql
-									</th>
-								</tr>
-							</table>
-						</div>
-					</div>
-				</div>
-			</div>
+        </div>
+    </div>
 
-			<hr class="my-4">
-			<p>
-				Agenda de contactos con php y mysql 
-				By Facultad Autodidacta
-			</p>
-			
-		</div>
-
-	</div>
-
-	
 </body>
+
 </html>
